080e6796d067326a2a66e764bd025a3058aeb081
GedMarc <ged_marc@hotmail.com>
2021-02-10 03:20:40 +0200

    UWE Assist V5 Updates and base corrections

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,6 +3,7 @@ module com.guicedee.activitymaster.imagemaster {
 	requires java.logging;
 	exports com.guicedee.activitymaster.imagemaster.services;
 	
+	requires com.guicedee.guicedservlets;
 	
 	requires static lombok;
 }
---
+++ src/main/java/com/guicedee/activitymaster/imagemaster/ImagePNGServlet.java
@@ -0,0 +1,10 @@
+package com.guicedee.activitymaster.imagemaster;
+
+import com.google.inject.Singleton;
+import jakarta.servlet.http.HttpServlet;
+
+@Singleton
+public class ImagePNGServlet extends HttpServlet
+{
+
+}
